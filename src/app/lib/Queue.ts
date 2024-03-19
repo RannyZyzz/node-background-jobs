60b58fd536bc1b1091057050a2be3c2b34122890
ranny-pop-os-22-04 <rannierreis@gmail.com>
2024-03-18 21:27:31 -0300

    pequeno erro em uma linha

--- src/app/lib/Queue.ts
+++ src/app/lib/Queue.ts
@@ -3,26 +3,26 @@ import redisConfig from '../../config/redis'
 import * as jobs from '../jobs'
 
 const queues = Object.values(jobs).map(job => ({
-    bull: new Queue(job.key, redisConfig),
-    name: job.key,
-    handle: job.handle
+  bull: new Queue(job.key, redisConfig),
+  name: job.key,
+  handle: job.handle
 }))
 
 export default {
   queues,
   add(name, data) {
     const queue = this.queues.find(queue => queue.name === name)
-    
+
     return queue.bull.add(data)
   },
-  process(){
+  process() {
     return this.queues.forEach(queue => {
       queue.bull.process(queue.handle)
-      
-      
+
+
       queue.bull.on('failed', (job, err) => {
         console.log('job failed', queue.name, job.data);
-        console.lgo(err)
+        console.log(err)
       })
     })
   }
